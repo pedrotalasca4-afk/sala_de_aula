f8892e1cd8170617b75b93c17536baf1de847d1b
PEDRO DE LIMA TALASCA <60110735064@senacrs.edu.br>
2026-09-21 21:32:36 -0300

    Ola Mundo

--- Exercícios/funcoes/basico.py/condicionais.py
+++ Exercícios/funcoes/basico.py/condicionais.py
@@ -1,12 +1,13 @@
 def fizz_buzz(numero=int):
-    if numero % 3 ==0:
-        return 'fizzbuz'
-    elif numero % 5 ==0:
-        return 'buzz'
-    elif numero % 3 == 0 and numero % 5 == 0:
+    if numero % 3 == 0 and numero % 5 == 0:
+        return 'fizzbuzz'
+    elif numero % 3 == 0:
         return 'fizz'
+    elif numero % 5 == 0:
+        return 'buzz'
     else:
         return numero
+
 
 #Exercicio 1
 def verificar_maioridade(idade:int):
@@ -15,13 +16,14 @@ def verificar_maioridade(idade:int):
     else:
         return 'Menor de idade'
 
+
 #Exercicio 2
 def verificar_paridade(numero:int):
     if numero % 2 ==0:
         return 'Par'
     else:
         return 'Impar'
-    
+   
 #Exercicio 3
 def classificar_numero(numero:float):
     if numero > 0:
@@ -30,7 +32,7 @@ def classificar_numero(numero:float):
         return 'Negativo'
     if numero == 0:
         return 'Zero'
-    
+   
 #Exercicio 4
 def calcular_resultado(nota1:float, nota2:float):
     resultado = (nota1+nota2)/2
@@ -38,7 +40,7 @@ def calcular_resultado(nota1:float, nota2:float):
         return 'Aprovado'
     if resultado < 7:
         return 'Reprovado'
-    
+   
 #Exercicio 5
 def maior_de_dois(a:int, b:int):
     if a > b:
@@ -47,41 +49,61 @@ def maior_de_dois(a:int, b:int):
         return 'O segundo é maior'
     if a == b:
         return 'São iguais'
-    
+   
 #Exercicio 6
 def calcular_desconto(valor_compra:float, cliente_vip:bool):
-    if cliente_vip == True or valor_compra > 200:
-        valor_final_d = (valor_compra*(15/100))
+    if cliente_vip or valor_compra > 200:
+        valor_final_d = valor_compra * 0.15
         desconto1 = valor_compra - valor_final_d
         return f'Valor Final: {desconto1:.2f}'
-    
-    if cliente_vip == False or valor_compra < 200:
-        valor_final_sd = (valor_compra*(5/100))
-        desconto2 = valor_compra - valor_final_sd
-        return f'Valor Final: {desconto2:.2f}'
-    
+
+
+    valor_final_sd = valor_compra * 0.05
+    desconto2 = valor_compra - valor_final_sd
+    return f'Valor Final: {desconto2:.2f}'
+   
 #Exercicio 7
 def conceito_nota(nota:float):
-    if nota >=9 or nota >=10:
+    if nota >= 9:
         return 'A'
-    if nota >=7 or nota >=8.9:
+    if nota >= 7:
         return 'B'
-    if nota >=5 or nota >=6.9:
+    if nota >= 5:
         return 'C'
-    if nota <5:
-        return 'F'
-    
+    return 'F'
+   
 #Exercicio 8
 def tipo_triangulo(a:int, b:int, c:int):
-    if (a+b>c) and (b+c>a) and (c+a>b):
-        if a == b == c or a == c == b:
-            return 'Equilatero'
-        if a == b != c or b==c!=a:
-            return 'Isóceles'
-        if a != b != c or a != c != b:
-            return 'Escaleno'
-        else:
-            return 'Não é um triangulo'
+    if not ((a + b > c) and (b + c > a) and (c + a > b)):
+        return 'Não é um triângulo'
+
+
+    if a == b == c:
+        return 'Equilatero'
+    if a == b or b == c or a == c:
+        return 'Isóceles'
+    return 'Escaleno'
+
+
+#Exercicio 9
+def calcular_imposto(salario:float):
+    if salario < 2000.00:
+        imposto1 = 0
+        return imposto1
+    if  salario > 2000.00:
+        imposto2 = (salario - 2000) * 0.10
+        return imposto2
+    if salario > 4000.00:
+        imposto3 = (2000*10) + (salario - 4000) * 20
+        return imposto3
+       
+#Exercicio 10
+def e_bissexto(ano:int):
+    if (ano % 4 == 0 and ano % 100 != 0) or (ano % 400 == 0):
+        return 'True'
+    else:
+        return 'False'
+
 
 if __name__=='__main__':
     teste = fizz_buzz(85)
@@ -100,6 +122,10 @@ if __name__=='__main__':
     print(f'6 - {cdes}')
     nota = conceito_nota(4.2)
     print(f'7 - {nota}')
-    trian = tipo_triangulo(1, 2, 1)
+    trian = tipo_triangulo(2, 5, 59)
     print(f'8 - {trian}')
+    imp = calcular_imposto(3000.0)
+    print(f'9 - {imp}')
+    bissexto = e_bissexto(1900)
+    print(f'10 - {bissexto}')
     pass
--- Exercícios/funcoes/loop.py
+++ Exercícios/funcoes/loop.py
@@ -1,4 +1,4 @@
-def dobrar(numeros:[]):
+def dobrar(numeros:list):
     for numero in numeros:
         numero = numero * 2
         print(numero)
@@ -9,10 +9,27 @@ def filtrar_pares(numeros:list):
     for numero in numeros:
         if numero % 2 == 0:
             pares.append(numero)
-
+        
     return pares
+
+#Exercicio 2
+def contar_negativos(numeros: list):
+    count = 0
+    for numero in numeros:
+        if numero < 0:
+            count+=1
+    return count
+
+#Exercicios 3
+def somar_maiores_que(numeros:list, limite:list):
+    count = limite
+    for numero in numeros:
+        if numero > limite:
+            count
 
 if __name__=='__main__':
     dobrar([1,2,3,4,5])
-    numeros_pares = filtrar_pares(1, 2, 3, 4, 5, 6)
-    print(f'{numeros_pares}')
+    numeros_pares = filtrar_pares([1, 2, 3, 4, 5, 6,])
+    print(f'1 -{numeros_pares}')
+    negativos = contar_negativos([10, -3, 0, -5, 8, -1])
+    print(f'2 -{negativos}')
